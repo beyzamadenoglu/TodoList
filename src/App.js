95d55db420cd0238d6170272cd55ab6fab90c6c2
Madenoglu, Beyza, Vodafone (External) <Beyza.Madenoglu@vodafone.com>
2023-01-26 22:26:00 +0300

    SASS implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Page from "./pages/TodoPage"
 import React from "react";
+import "./styles/index.scss";
 
 
 function App() {
   return (
     <div className="App">
+      <h2>beyzam</h2>
       <Page />
     </div>
   );
